bb5ef644bf7d41ec554c59a96279e5f3e4a163e3
fhollenbach <florianhollenbach@west-10-232-135-27.tamulink.tamu.edu>
2018-12-07 14:42:29 -0600

    Model Tables and comparisons

--- Code/model_comparison.R
+++ Code/model_comparison.R
@@ -53,14 +53,96 @@ info_crit <- function(model){
 
 
 
+##model HM
+vars <- get_variables(model_HM)[c(1:12, 30)]
 
-vars <- get_variables(model_HM)
+model.HM.95 <- model_HM %>% spread_draws(b_Intercept, b_L_Polity_s_interp, b_L_Fiscal_Rel_interp, b_D_Fiscal_Rel_Interp, b_L_D_Fiscal_Rel_Interp, b_L_logGDPPERCAP, b_L_CivilWar, b_L_REGION_DEM_DIFFUSE, b_L_WORLD_DEM_DIFFUSE, b_D_GDPPERCAP, b_D_RegionalDiffusion, b_D_WORLD_DEM_DIFFUSE, sigma) %>% mutate(longRun_fiscal =  b_L_Fiscal_Rel_interp /abs(b_L_Polity_s_interp)) %>% median_qi(.width = c(0.95))
+vars  <- c(vars[1:12], "longRun_fiscal", vars[13])
 
-model.HM <- model_HM %>% spread_draws(b_Intercept, b_L_Polity_s_interp, b_L_Fiscal_Rel_interp, b_D_Fiscal_Rel_Interp, b_L_D_Fiscal_Rel_Interp, b_L_logGDPPERCAP, b_L_CivilWar, b_L_REGION_DEM_DIFFUSE, b_L_WORLD_DEM_DIFFUSE, b_D_GDPPERCAP, b_D_RegionalDiffusion, b_D_WORLD_DEM_DIFFUSE) %>% mutate(longRun_fiscal =  b_L_Fiscal_Rel_interp /abs(b_L_Polity_s_interp)) %>% median_qi(.width = c(0.5))
-m
-mcmc_intervals(select(model.HM, -c(.chain, .iteration, .draw)))
+est  <-round( model.HM.95[vars], 3)
+CI.low <- matrix(as.numeric(model.HM.95[paste(vars, "lower", sep = ".")]), ncol = 1)
+row.names(CI.low) <- vars
+CI.high <- matrix(as.numeric(model.HM.95[paste(vars, "upper", sep = ".")]), ncol = 1)
+row.names(CI.high) <- vars
+CI <- data.frame(cbind(CI.low,CI.high))
+CI$interval <- paste("(", round(CI$X1,3), ",", round(CI$X2,3), ")", sep = "")
+
+col.HM <- unlist(sapply(seq_along(est), function(i) append(est[i], CI$interval[i], i)))
+col.HM <- c(col.HM[1:24], rep(NA, 16), col.HM[25:28]) ### add 6 rows for AR, 6 for ineq & 4 for lag model
 
 
+
+
+##model AR
+vars <- get_variables(model_AR)[c(1:15, 33)]
+
+model.AR.95 <- model_AR %>% spread_draws(b_Intercept, b_L_Polity_s_interp, b_L_Fiscal_Rel_interp, b_D_Fiscal_Rel_Interp, b_L_D_Fiscal_Rel_Interp, b_L_logGDPPERCAP, b_L_CivilWar, b_L_REGION_DEM_DIFFUSE, b_L_WORLD_DEM_DIFFUSE, b_D_GDPPERCAP, b_D_RegionalDiffusion, b_D_WORLD_DEM_DIFFUSE, b_post1980, b_post1980_L_Fiscal_Rel_interp, b_post1980_D_Fiscal_Rel_Interp, sigma) %>% mutate(longRun_fiscal =  b_L_Fiscal_Rel_interp /abs(b_L_Polity_s_interp)) %>% median_qi(.width = c(0.95))
+vars  <- c(vars[1:15], "longRun_fiscal", vars[16])
+
+est  <- round(model.AR.95[vars], 3)
+CI.low <- matrix(as.numeric(model.AR.95[paste(vars, "lower", sep = ".")]), ncol = 1)
+row.names(CI.low) <- vars
+CI.high <- matrix(as.numeric(model.AR.95[paste(vars, "upper", sep = ".")]), ncol = 1)
+row.names(CI.high) <- vars
+CI <- data.frame(cbind(CI.low,CI.high))
+CI$interval <- paste("(", round(CI$X1,3), ",", round(CI$X2,3), ")", sep = "")
+
+col.AR <- unlist(sapply(seq_along(est), function(i) append(est[i], CI$interval[i], i)))
+col.AR <- c(col.AR[1:30], rep(NA, 10), col.AR[31:34]) ### add 6 rows for ineq & 4 for lag model
+
+
+
+##model ineq
+vars <- get_variables(model_ineq)[c(1:15, 33)]
+
+model.ineq.95 <- model_ineq %>% spread_draws(b_Intercept, b_L_Polity_s_interp, b_L_Fiscal_Rel_interp, b_D_Fiscal_Rel_Interp, b_L_D_Fiscal_Rel_Interp, b_L_logGDPPERCAP, b_L_CivilWar, b_L_REGION_DEM_DIFFUSE, b_L_WORLD_DEM_DIFFUSE, b_D_GDPPERCAP, b_D_RegionalDiffusion, b_D_WORLD_DEM_DIFFUSE, b_very_unequal_utip, b_unequal_L_Fiscal_Rel_interp, b_unequal_D_Fiscal_Rel_Interp, sigma) %>% mutate(longRun_fiscal =  b_L_Fiscal_Rel_interp /abs(b_L_Polity_s_interp)) %>% median_qi(.width = c(0.95))
+vars  <- c(vars[1:15], "longRun_fiscal", vars[16])
+
+est  <- round(model.ineq.95[vars], 3)
+CI.low <- matrix(as.numeric(model.ineq.95[paste(vars, "lower", sep = ".")]), ncol = 1)
+row.names(CI.low) <- vars
+CI.high <- matrix(as.numeric(model.ineq.95[paste(vars, "upper", sep = ".")]), ncol = 1)
+row.names(CI.high) <- vars
+CI <- data.frame(cbind(CI.low,CI.high))
+CI$interval <- paste("(", round(CI$X1,3), ",", round(CI$X2,3), ")", sep = "")
+
+col.ineq <- unlist(sapply(seq_along(est), function(i) append(est[i], CI$interval[i], i)))
+col.ineq <- c(col.ineq[1:24], rep(NA, 6), col.ineq[25:30], rep(NA, 4), col.ineq[31:34] ) ### add 6 rows for AR & 4 for lag model
+
+
+##model lag
+vars <- get_variables(model_lag)[c(1:14, 32)]
+
+model.lag.95 <- model_lag %>% spread_draws(b_Intercept, b_L_Polity_s_interp, b_L_Fiscal_Rel_interp, b_D_Fiscal_Rel_Interp, b_L_D_Fiscal_Rel_Interp, b_L_logGDPPERCAP, b_L_CivilWar, b_L_REGION_DEM_DIFFUSE, b_L_WORLD_DEM_DIFFUSE, b_D_GDPPERCAP, b_D_RegionalDiffusion, b_D_WORLD_DEM_DIFFUSE, b_polity_L_Fiscal_Rel_interp, b_polity_D_Fiscal_Rel_Interp, sigma) %>% mutate(longRun_fiscal =  b_L_Fiscal_Rel_interp /abs(b_L_Polity_s_interp)) %>% median_qi(.width = c(0.95))
+
+vars  <- c(vars[1:14], "longRun_fiscal", vars[15])
+est  <- round(model.lag.95[vars], 3)
+CI.low <- matrix(as.numeric(model.lag.95[paste(vars, "lower", sep = ".")]), ncol = 1)
+row.names(CI.low) <- vars
+CI.high <- matrix(as.numeric(model.lag.95[paste(vars, "upper", sep = ".")]), ncol = 1)
+row.names(CI.high) <- vars
+CI <- data.frame(cbind(CI.low,CI.high))
+CI$interval <- paste("(", round(CI$X1,3), ",", round(CI$X2,3), ")", sep = "")
+
+col.lag <- unlist(sapply(seq_along(est), function(i) append(est[i], CI$interval[i], i)))
+col.lag <- c(col.lag[1:24], rep(NA, 12),col.lag[25:28], col.ineq[29:32]) ### add 6 rows for AR & Ineq model
+
+
+length(col.HM)
+length(col.AR)
+length(col.ineq)
+length(col.lag)
+
+vars <-tibble("Intercept", "Polity t-1", "Fiscal Reliance t-1", "$Delta$ Fiscal Reliance", "$Delta$ Fiscal Reliance t-1", "log GDP pC t-1", "Civil War t-1", "Regional dem diffusion t-1", "Global dem diffusion t-1", "$Delta$ log GDP pC", "$Delta$ Regional Dem Diffusion", "$Delta$ Global Dem Diffusion","post 1980", "post 1980 $times$ Fisc Rel t-1", "post 1980 $times$ $Delta$ Fisc Rel", "Inequality", "Inequality $times$ Fisc Rel t-1", "Inequality $times$ $Delta$ Fisc Rel", "Polity t-1 $times$ Fisc Rel t-1", "Polity t-1 $times$ $Delta$ Fisc Rel","Long Run Fisc Rel", "Sigma")
+vars_ci <- rep(NA, 22)
+names <- unlist(sapply(seq_along(vars), function(i) append(vars[i], vars_ci[i], i)))
+
+results <- tibble(Variable = names, HM = col.HM, AR = col.AR, Inequality = col.ineq, Polity = col.lag)
+
+
+
+tab  <- xtable(results,  caption = "Model Results and 95 Credible Intervals", label = "tab:models", align = "llcccc")
+print(tab, include.rownames = FALSE, booktabs = TRUE)
 
 ### deivance based on median estimate aic, dic, bic,
 info_HM  <- info_crit(model_HM)
@@ -96,7 +178,3 @@ models_waic <- bind_cols(Model, waic)
 table  <- xtable(models_waic, digits = 2, caption = "WAIC for all Models", label = "tab:waic", align = "llcc")
 print(table, include.rownames = FALSE, booktabs = TRUE)
 
-
-
-#### bayesfactor
-    bayes_factor(model_HM, model_AR, model_ineq, model_lag)
