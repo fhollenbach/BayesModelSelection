66d5f9ed5d77b6add92c77dd69bece9d23456a65
fhollenbach <florianhollenbach@Florians-MacBook-Pro.local>
2019-01-08 08:39:59 -0600

    new application tables

--- Code/pivot_models.R
+++ Code/pivot_models.R
@@ -5,7 +5,7 @@ library(tidyverse)
 library(tictoc)
 library(rethinking)
 library(haven)
-
+library(sjstats)
 data <- read_dta("~/Dropbox/BayesChapter/Data/PartiesPivotsPolicies/PartiesPivotsandPolicyReplicationDataV2.dta")
 
 summary(data)
@@ -54,25 +54,59 @@ save(model6, file = "~/Dropbox/BayesChapter/Model_Results/model6.rda")
 
 #### mnodel 6 but with horseshoe
 
-scaled <- as.tibble(scale(select(model.data.horse, -c(hmccode,year,post1980,gdp_verylow_firstoil_year,32:48))))
-
-model.data.scaled <- bind_cols(select(model.data.horse, c(hmccode,year,post1980,gdp_verylow_firstoil_year)), scaled)
-model.data.scaled <- bind_cols(model.data.scaled, select(model.data.horse, c(32:48)))
-
-
+data <- read_dta("~/Dropbox/BayesChapter/Data/PartiesPivotsPolicies/PartiesPivotsandPolicyReplicationDataV2.dta")
 summary(data)
-ratio <- 7/(dim(data)[2]-1)
 
-#writeLines(readLines("~/Documents/GitHub/BayesModelSelection/Code/linear_regHorseshoe.stan"))
-n <- dim(model.data.scaled)[1]
-X <- as.matrix(select(model.data.scaled,-c(hmccode, year, D_polity_s_interp)))
-data_stan <- list(y = model.data.scaled$D_polity_s_interp, K = dim(X)[2], X = t(X), m_exp = 10)
-
-rstan_options(auto_write = TRUE) 
-options(mc.cores = parallel::detectCores())
-
-finnish_fit <- stan(file='~/Documents/GitHub/BayesModelSelection/Code/linear_regHorseshoe.stan', init = 0,data=data_stan, seed=12345, iter = 5000, control=list(adapt_delta=0.99, max_treedepth=15), cores = 4)
+data  <- data %>% filter(laggedsq < 1 & laggedsq > -1)
+data  <- data %>% group_by(issue) %>% mutate(L_sqtransform = lag(sqtransform, order_by = congress))
 
 
+data <- data %>% select(c(sqtransform,L_sqtransform, transfloormedianslocationpredict, transfpivotalgridlocklocationpre, transfpartycartelopenrule, transfpartycartelclosedrulepred, transinflxmedgrid, transinflxpartunitblock, congress, issue, presidentiallocation, housemedian, senatefilibusterpivot, senatemedian, housevetopivot, senatevetopivot, housemajoritymedian, senatemajoritymedian, houseminoritymedian, senateminoritymedian))
+
+                                        #writeLines(readLines("~/Documents/GitHub/BayesModelSelection/Code/linear_regHorseshoe.stan"))
+data <- data %>% ungroup()
+data  <- na.omit(data)
 
 
+scaled <- as.tibble(scale(select(data, -c( congress, issue))))
+model.data.scaled <- bind_cols(select(data, c(congress, issue)), scaled)
+
+
+X <- as.matrix(select(model.data.scaled,-c(sqtransform, congress, issue)))
+
+ratio = 2 / (4)
+prior  <- set_prior(horseshoe(df = 1, par_ratio = ratio))
+horseshoe <- brm(formula = sqtransform ~ - 1 + transfloormedianslocationpredict +transfpivotalgridlocklocationpre + transfpartycartelopenrule +transfpartycartelclosedrulepred + transinflxmedgrid + transinflxpartunitblock, data =model.data.scaled, family = gaussian, warmup = 2000, iter = 4000, chains = 4, cores = 4, save_all_pars =T, control = list(adapt_delta = 0.99), seed = 12345, prior = prior)
+save(horseshoe, file = "~/Dropbox/BayesChapter/Model_Results/horseshoe.rda")
+
+prior  <- set_prior(horseshoe(df = 1, par_ratio = ratio, scale_slab = 3, df_slab = 25))
+fin_horseshoe <- brm(formula = sqtransform ~ - 1 + transfloormedianslocationpredict +transfpivotalgridlocklocationpre + transfpartycartelopenrule +transfpartycartelclosedrulepred + transinflxmedgrid + transinflxpartunitblock, data =model.data.scaled, family = gaussian, warmup = 2000, iter = 4000, chains = 4, cores = 4, save_all_pars =T, control = list(adapt_delta = 0.99), seed = 12345, prior = prior)
+save(fin_horseshoe, file = "~/Dropbox/BayesChapter/Model_Results/fin_horseshoe.rda")
+
+
+color_scheme_set("red")
+ppc_dens_overlay(y = c(model.data.scaled$sqtransform),
+                 yrep = posterior_predict(fin_horseshoe, draws = 500))
+ggsave("~/Dropbox/Apps/Overleaf/Bayesian model selection and averaging/fin_horseshoe_pdens.pdf", width = 10 * 1.618, height = 10)
+
+
+forTable <- tidy_stan(fin_horseshoe, prob = c(0.9, 0.95))[1:7, ]
+vars <- c( "Median only", "Pivotal politics", "Party cartel open rule", "Party cartel closed rule", "Inflation (median/pivot)", "Inflation (party)", "Sigma")
+forTable <- forTable %>% select(c("term", "estimate", "std.error", "hdi.low_0.9", "hdi.high_0.9", "hdi.low_0.95", "hdi.high_0.95", "rhat"))
+names(forTable) <- c("Variable", "Mean", "Std Error", "lower 90 HDI", "upper 90 HDI", "lower 95 HDI", "upper 90 HDI", "Rhat")
+forTable$Variable <- vars
+forTable <- as.matrix(forTable)
+colnames(forTable) <- c(" ", " ", " ", "lower 90$\\%$", "upper  $90\\%$", "lower  $90\\%$", "upper  $90\\%$", " ")
+forTable <- rbind(c(" ", "Mean", "Std Error", "HDI", "HDI", "HDI", "HDI", "Rhat"), forTable)
+                                        #names(forTable)[1] <- ""
+
+print(xtable(forTable, caption = "Model Results: Finnish Horseshoe Model", label = "tab:horseshoe", floating = TRUE, latex.environments = "center", align = "llccccccc", digits = 2), caption.placement = "top", booktabs = TRUE, include.rownames = FALSE,  sanitize.text.function = function(x) {x}, hline.after = c(-1, 1,nrow(forTable)), file = "~/Dropbox/Apps/Overleaf/Bayesian model selection and averaging/horseshoe_coefs.tex")
+
+### model normal priors on scaled
+prior = c(set_prior("normal(0,5)", class = "b"))
+model_scaled <- brm(formula = sqtransform ~ - 1 + transfloormedianslocationpredict +transfpivotalgridlocklocationpre + transfpartycartelopenrule +transfpartycartelclosedrulepred + transinflxmedgrid + transinflxpartunitblock, data = data, family = gaussian, warmup = 2000, iter = 4000, chains = 4, cores = 4, save_all_pars =T, control = list(adapt_delta = 0.99), seed = 12345, prior = prior)
+model_scaled
+color_scheme_set("blue")
+ppc_dens_overlay(y = c(model.data.scaled$sqtransform),
+                 yrep = posterior_predict(model_scaled, draws = 500))
+ggsave("~/Dropbox/Apps/Overleaf/Bayesian model selection and averaging/model_pdens.pdf", width = 10 * 1.618, height = 10)
